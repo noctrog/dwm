e9f5609c6b509b3e24ac62da3a3376c368dd848c
noctrog <ramon1999cg@gmail.com>
2019-05-23 16:34:45 +0200

    Gruvbox

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#161819";
+static const char col_gray2[]       = "#3c3836";
+static const char col_gray3[]       = "#d5c4a1";
+static const char col_gray4[]       = "#fbf1c7";
+static const char col_cyan[]        = "#161819";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
