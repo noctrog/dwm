538624f734a6a5f7cbea72f6c810a66ff15eaf66
noctrog <ramon1999cg@gmail.com>
2019-06-07 19:14:43 +0200

    shortcuts

--- config.h
+++ config.h
@@ -31,7 +31,6 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Telegram",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Rambox",  NULL,       NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
@@ -63,13 +62,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *fexpcmd[]  = { "st -e ranger", NULL };
+static const char *calendar[] = {"st", "-e", "calcurse", NULL};
+static const char *mus_player[] = {"st", "-e", "cmus", NULL};
+static const char *file_manager[] = {"st", "-e", "ranger", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_f,	   spawn,          {.v = fexpcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd} },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd} },
+	{ MODKEY|ShiftMask,		XK_k,      spawn,	   {.v = calendar} },
+	{ MODKEY|ShiftMask,		XK_m,      spawn,	   {.v = mus_player} },
+	{ MODKEY|ShiftMask,		XK_f,      spawn,	   {.v = file_manager} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
