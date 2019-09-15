6e1512022271cd71583985d8a5ad4b908e2353a3
noctrog <ramon1999cg@gmail.com>
2019-09-15 19:38:47 +0200

    Gaps patch

--- config.h
+++ config.h
@@ -2,10 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx	    = 10;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Inconsolata:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#161819";
 static const char col_gray2[]       = "#3c3836";
@@ -29,7 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Telegram",  NULL,       NULL,       1 << 6,       0,           -1 },
 	{ "Steam",  NULL,       NULL,       1 << 5,       0,           -1 },
 };
